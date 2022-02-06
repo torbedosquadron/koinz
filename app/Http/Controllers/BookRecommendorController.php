2639bc06aaebc4f8c0de0dfab4a3b72ac0bfde5b
Mohamed Fathy <torbedosquadron8@gmail.com>
2022-02-06 16:41:17 +0200

    Adding Controllers classes

---
+++ app/Http/Controllers/BookRecommendorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\SubmitReadingIntervalRequest;
+use App\Models\PageRead;
+use App\Models\Book;
+use App\Http\Resources\BookResource;
+use App\Libraries\AmazonSMSProvider;
+use App\Libraries\InfopipSMSProvider;
+
+
+class BookRecommendorController extends Controller
+{
+    /**
+     * API to submit a user reading interval
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function SubmitReadingInterval (SubmitReadingIntervalRequest $request)
+    {
+        
+        $PageRead = new PageRead;
+
+        $PageRead->user_id    = $request->user_id;
+        $PageRead->book_id    = $request->book_id;
+        $PageRead->start_page = $request->start_page;
+        $PageRead->end_page   = $request->end_page;
+
+        $PageRead->save();
+
+        // the credentials should be put in .env file
+        // but for ease of use and it will not be commited 
+        // it is hard coded in this class.
+        // $SMSProvider = new AmazonSMSProvider('anonymous', 'mEwUoo21UNSfSpLgXqhmG0pPNDa0');
+        // $SMSProvider->SMS("Thank you for your feedback");
+
+
+        $SMSProvider = new InfopipSMSProvider('anonymous', 'password', 'mEwUoo21UNSfSpLgXqhmG0pPNDa0');
+        $SMSProvider->SMS("Thank you for your feedback");
+
+        return response()->json([
+            'status'  => 200,
+            'message' => 'success'
+        ]);
+    } 
+    
+    /**
+     * API to Calculate the most recommended five books
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function GetRecommendedBooks (Request $request)
+    {
+        $Books = Book::orderByDesc('read_pages_num')->take(5)->get();
+
+        return response()->json([
+            'status'       => 200,
+            'message'      => 'success',
+            'data'         =>  BookResource::collection($Books),
+        ]);
+    }
+}
