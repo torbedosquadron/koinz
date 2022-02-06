bad897654e7f942e85d79d30de803f02cd6c5303
Mohamed Fathy <torbedosquadron8@gmail.com>
2022-02-06 16:30:31 +0200

    Adding Models classes

---
+++ app/Models/PageRead.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PageRead extends Model
+{
+    use HasFactory;
+
+    protected $table      = 'pages_read';
+}
